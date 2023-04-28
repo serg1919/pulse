8d0147f8e6a37e2ed64438f94f8e1bba1b24bcd4
serg1919 <fre_@ukr.net>
2023-04-28 20:14:48 +0300

    add tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,7 @@
 "use strict"
 
-// window.onload = function () {
-//   // document.body.style.backgroundColor = 'red';
+// Slick slider
 
-
-// };
 $(window).on('load', function () {
   $('.polar-slider').slick({
     slidesToShow: 1,
@@ -23,4 +20,29 @@ $(window).on('load', function () {
       }
     ]
   });
+
+  // jQuery - скрипт для табов(вкладок)
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.catalog__page-wrap').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+
+      })
+    })
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
 });
