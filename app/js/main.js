6c555bdd74e5e3da0f815135898fedc5077e64a1
serg1919 <fre_@ukr.net>
2023-04-29 20:37:34 +0300

    add js

--- app/js/main.js
+++ app/js/main.js
@@ -31,18 +31,26 @@ $(window).on('load', function () {
 
 
 
-  function toggleSlide(item) {
-    $(item).each(function (i) {
+
+  $('.catalog-item__link').each(function (i) {
       $(this).on('click', function (e) {
         e.preventDefault();
         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-
       })
-    })
-  };
+    });
 
-  toggleSlide('.catalog-item__link');
-  toggleSlide('.catalog-item__back');
 
 });
+
+
+  // if ($('.catalog-item__list').eq(i).hasClass('catalog-item__list_active')) {
+  //   $(this).eq(i).text('Назад');
+  // } else {
+  //   $(this).eq(i).text('Детальніше');
+  // }
+
+
+  // $('.catalog-item__list_active').prop('value', 'Назад');
+// $(i) = element.textContent.replace('Детальніше', 'Назад');
+// toggleSlide('.catalog-item__back');
